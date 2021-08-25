84b6fcaa17adf78a1163d30f3a06c45d396b50a6
U-CONSERTO\romain.guilbaud <Romain Guilbaud>
2021-08-25 18:24:17 +0200

    ajout script xml to bdd

---
+++ my_project/migrations/Version20210825135642.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210825135642 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE item (id INT AUTO_INCREMENT NOT NULL, product_id INT DEFAULT NULL, order_item_id INT DEFAULT NULL, quantity INT NOT NULL, INDEX IDX_1F1B251E4584665A (product_id), INDEX IDX_1F1B251EE415FB15 (order_item_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `order` (id INT AUTO_INCREMENT NOT NULL, customer_id INT NOT NULL, order_date DATETIME NOT NULL, status VARCHAR(255) NOT NULL, price DOUBLE PRECISION NOT NULL, INDEX IDX_F52993989395C3F3 (customer_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, sku VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, price DOUBLE PRECISION NOT NULL, UNIQUE INDEX UNIQ_D34A04ADF9038C4 (sku), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, user_name VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE item ADD CONSTRAINT FK_1F1B251E4584665A FOREIGN KEY (product_id) REFERENCES product (id)');
+        $this->addSql('ALTER TABLE item ADD CONSTRAINT FK_1F1B251EE415FB15 FOREIGN KEY (order_item_id) REFERENCES `order` (id)');
+        $this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_F52993989395C3F3 FOREIGN KEY (customer_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE item DROP FOREIGN KEY FK_1F1B251EE415FB15');
+        $this->addSql('ALTER TABLE item DROP FOREIGN KEY FK_1F1B251E4584665A');
+        $this->addSql('ALTER TABLE `order` DROP FOREIGN KEY FK_F52993989395C3F3');
+        $this->addSql('DROP TABLE item');
+        $this->addSql('DROP TABLE `order`');
+        $this->addSql('DROP TABLE product');
+        $this->addSql('DROP TABLE user');
+    }
+}
---
+++ my_project/migrations/Version20210825135650.php
@@ -0,0 +1,112 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210825135650 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // xml file path
+        $path = "/var/www/orders.xml";
+        $xmlfile = file_get_contents($path);
+        $new = simplexml_load_string($xmlfile);
+        $con = json_encode($new);
+        $newArr = json_decode($con, true);
+        $userArray = array();
+        $itemArray = array();
+        $productArray = array();
+        $orderArray = array();
+        foreach ($newArr['order'] as $order) {
+            $idOrder = $order["@attributes"]["id"];
+            $idUser = $order["customer"]["@attributes"]["id"];
+            if (!isset($userArray[$idUser])) {
+                $userArray[$idUser] = [
+                    "id" => $idUser,
+                    "firstname" => $order["customer"]["firstname"],
+                    "lastname" => $order["customer"]["lastname"]
+                ];
+            }
+            foreach($order["cart"] as $product) {
+                // if only one product
+                if (isset($product["@attributes"])) {
+                    $this->setProductArrayAndItemArray($productArray, $itemArray, $product, $idOrder);
+                } else {
+                    foreach ($product as $p) {
+                        $this->setProductArrayAndItemArray($productArray, $itemArray, $p, $idOrder);
+                    }
+                }
+            }
+            $orderArray[] = [
+                "id" => $idOrder,
+                "customer" => $idUser,
+                "status" => $order["status"],
+                "price" => $order["price"],
+                "orderDate" => $order["orderDate"]
+            ];
+        }
+        $this->persistInBdd($userArray, $productArray, $itemArray, $orderArray);
+    }
+
+    public function down(Schema $schema): void
+    {
+
+    }
+
+    private function setProductArrayAndItemArray(
+        array &$productArray,
+        array &$itemArray,
+        array $product,
+        string $idOrder
+    ) {
+        $idProduct = $product["@attributes"]["sku"];
+        if (!isset($productArray[$idProduct])) {
+            $productArray[$idProduct] = [
+                "sku" => $idProduct,
+                "name" => $product["name"],
+                "price" => $product["price"]
+            ];
+        }
+        $itemArray[] = [
+            "product" => $idProduct,
+            "quantity" => $product["quantity"],
+            "order" => $idOrder
+        ];
+    }
+
+    private function persistInBdd ($userArray, $productArray, $itemArray, $orderArray) {
+        $id = 1;
+        foreach ($productArray as $product) {
+            $this->addSql('INSERT INTO product (id, sku, name, price) VALUES (' . $id . ', \'' . $product["sku"] . '\', \'' . $product["name"] . '\', ' . $product["price"] . ')');
+            $id++;
+        }
+        $this->addSql('INSERT INTO user (id, first_name, last_name, user_name, password) VALUES (' . $id . ', \'admin\', \'admin\', \'admin\', \'S3cr3T+\')');
+        $id = 1;
+        foreach ($userArray as $user) {
+            $this->addSql('INSERT INTO user (id, first_name, last_name, user_name, password) VALUES (' . $id . ', \'' . $user["firstname"] . '\', \'' . $user["lastname"] . '\', \'' . $user["firstname"] . '_' . $user["lastname"] . '\', \'1234\')');
+            $id++;
+        }
+        $id = 1;
+        foreach ($orderArray as $order) {
+            $this->addSql('INSERT INTO `order` (id, customer_id, order_date, status, price) VALUES (' . $id . ', \'' . $order["customer"] . '\', \'' . $order["orderDate"] . '\', \'' . $order["status"] . '\', ' . $order["price"] . ')');
+            $id++;
+        }
+        $id = 1;
+        foreach ($itemArray as $item) {
+            $this->addSql('INSERT INTO item (id, product_id, order_item_id, quantity) VALUES (' . $id . ', ' . $item["product"] . ', ' . $item["order"] . ', ' . $item["quantity"] . ')');
+            $id++;
+        }
+    }
+}
